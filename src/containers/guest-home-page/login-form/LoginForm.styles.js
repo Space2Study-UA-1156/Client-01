889592fa7cc0a5d6e9dcb7374b1d7edf21edfdf7
YaroslavChuiko <jarik.tchuicko@gmail.com>
2024-04-25 15:15:18 +0300

    update LoginForm width styles

--- src/containers/guest-home-page/login-form/LoginForm.styles.js
+++ src/containers/guest-home-page/login-form/LoginForm.styles.js
@@ -2,7 +2,7 @@ export const styles = {
   form: {
     display: 'flex',
     flexDirection: 'column',
-    minWidth: { sm: '340px' }
+    maxWidth: { md: '343px' }
   },
   input: {
     maxWidth: '343px'
