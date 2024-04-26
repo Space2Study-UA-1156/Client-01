788c08418cc140f7d7870f3ea6e2d61cef503d62
Людмила Кирюшина <o.v.ovsiyenko@gmail.com>
2024-04-26 16:55:06 +0300

    refacore naming

--- src/containers/guest-home-page/how-it-works/HowItWorks.jsx
+++ src/containers/guest-home-page/how-it-works/HowItWorks.jsx
@@ -18,10 +18,10 @@ import { styles } from '~/containers/guest-home-page/how-it-works/HowItWorks.sty
 const HowItWorks = () => {
   const { t } = useTranslation()
 
-  const [isStudent, setIsStudent] = useState(false)
+  const [isTutor, setIsTutor] = useState(false)
 
   const onChange = () => {
-    setIsStudent(!isStudent)
+    setIsTutor(!isTutor)
   }
 
   const switchOptions = {
@@ -40,21 +40,21 @@ const HowItWorks = () => {
           {t('guestHomePage.howItWorks.title')}
         </Typography>
         <AppContentSwitcher
-          active={isStudent}
+          active={isTutor}
           onChange={onChange}
           styles={styles.switch}
           switchOptions={switchOptions}
           typographyVariant={'h6'}
         />
         <CardsWithButton
-          array={isStudent ? tutorCardBoxArray : studentCardBoxArray}
+          array={isTutor ? tutorCardBoxArray : studentCardBoxArray}
           btnText={
-            !isStudent
+            !isTutor
               ? t('guestHomePage.whatCanYouDo.learn.actionLabel')
               : t('guestHomePage.whatCanYouDo.teach.actionLabel')
           }
-          isStudent={isStudent}
-          role={isStudent ? tutor : student}
+          isStudent={isTutor}
+          role={isTutor ? tutor : student}
         />
       </Box>
     </Box>
