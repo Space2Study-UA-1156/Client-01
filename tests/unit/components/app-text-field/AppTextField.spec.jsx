170810e8cda3f0bc8be20876dcc1256375109a40
YaroslavChuiko <jarik.tchuicko@gmail.com>
2024-05-01 20:58:53 +0300

    add input without error message test scenario

--- tests/unit/components/app-text-field/AppTextField.spec.jsx
+++ tests/unit/components/app-text-field/AppTextField.spec.jsx
@@ -1,20 +1,33 @@
 import { render, screen } from '@testing-library/react'
-import { beforeEach, describe, it } from 'vitest'
+import { vi } from 'vitest'
 import AppTextField from '~/components/app-text-field/AppTextField'
 
-describe('AppTextField test', () => {
-  const props = {
-    errorMsg: 'error message',
-    label: 'label'
+vi.mock('@mui/material/Tooltip', () => {
+  return {
+    default: ({ title, children }) => <span title={title}>{children}</span>
   }
-  beforeEach(() => {
-    render(<AppTextField {...props} />)
-  })
+})
+vi.mock('@mui/material/Typography', () => {
+  return {
+    default: ({ variant, children }) => (
+      <span data-testid={variant}>{children}</span>
+    )
+  }
+})
 
+describe('AppTextField test', () => {
   it('should render input with error message', () => {
+    render(<AppTextField errorMsg='error message' label='label' />)
     const inputElement = screen.getByLabelText(/label/i)
-    const errorMessage = screen.getByText(/error message/i)
+    const errorMessage = screen.getByTitle(/error message/i)
     expect(inputElement).toBeInTheDocument()
     expect(errorMessage).toBeInTheDocument()
+  })
+  it('should render input without error message', () => {
+    render(<AppTextField errorMsg={''} label='label' />)
+    const inputElement = screen.getByLabelText(/label/i)
+    const errorMessage = screen.queryByTitle('')
+    expect(inputElement).toBeInTheDocument()
+    expect(errorMessage).toBeNull()
   })
 })
