84a9509e7c946c1c695b502064b5880beab1f69f
YaroslavLys <74725159+YaroslavLys@users.noreply.github.com>
2024-05-01 12:45:18 +0300

    Mocked PageWrapper and used getByTestId

--- tests/unit/pages/subjects/Subjects.spec.jsx
+++ tests/unit/pages/subjects/Subjects.spec.jsx
@@ -1,11 +1,19 @@
-import { renderWithProviders } from '~tests/test-utils'
-import Subjects from '~/pages/subjects/Subjects'
 import { screen } from '@testing-library/react'
+import { vi } from 'vitest'
+import Subjects from '~/pages/subjects/Subjects'
+import { renderWithProviders } from '~tests/test-utils'
+
+vi.mock('~/components/page-wrapper/PageWrapper', () => ({
+  __esModule: true,
+  default: vi.fn(({ children }) => (
+    <div data-testid='page-title'>{children}</div>
+  ))
+}))
 
 describe('SubjectPage test', () => {
   it('It should render page', () => {
     renderWithProviders(<Subjects />)
-    const pageTitle = screen.getByText('Subjects')
+    const pageTitle = screen.getByTestId('page-title')
     expect(pageTitle).toBeInTheDocument()
   })
   it('Text of the page must be in the document', () => {
