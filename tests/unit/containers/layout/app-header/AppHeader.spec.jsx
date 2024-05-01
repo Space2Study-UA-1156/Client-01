54e3328c8045f611ab0971ee4ab0a81356c7edac
Vitalii Voloshyn <voloshin.v.02@gmail.com>
2024-05-01 18:54:35 +0300

    mock NavBar

--- tests/unit/containers/layout/app-header/AppHeader.spec.jsx
+++ tests/unit/containers/layout/app-header/AppHeader.spec.jsx
@@ -1,6 +1,12 @@
 import { screen } from '@testing-library/react'
 import { renderWithProviders } from '~tests/test-utils'
+import { vi } from 'vitest'
 import AppHeader from '~/containers/layout/app-header/AppHeader'
+
+vi.mock('~/containers/layout/navbar/NavBar', () => ({
+  __esModule: true,
+  default: () => <div>NavBar</div>
+}))
 
 describe('AppHeader container', () => {
   it('should render toolbar', () => {
