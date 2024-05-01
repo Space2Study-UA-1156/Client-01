cc97c5bc83d07911e8d5305f4f5d9f2b79df9150
Vadym Khytryk <khytrykv@gmail.com>
2024-05-01 10:19:15 +0300

    Fix comments

--- tests/unit/pages/categories/Categories.spec.jsx
+++ tests/unit/pages/categories/Categories.spec.jsx
@@ -1,25 +1,17 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import { vi } from 'vitest'
 
-import { ModalProvider } from '~/context/modal-context'
 import Categories from '~/pages/categories/Categories'
+import { renderWithProviders } from '~tests/test-utils'
 
-const mockCloseModal = vi.fn()
-
-vi.mock('~/context/modal-context', () => ({
-  ModalProvider: ({ children }) => <div>{children}</div>,
-  useModalContext: () => ({
-    closeModal: mockCloseModal
-  })
+vi.mock('~/components/page-wrapper/PageWrapper', () => ({
+  __esModule: true,
+  default: vi.fn(({ children }) => <div>{children}</div>)
 }))
 
 describe('Categories', () => {
   it('renders correctly', async () => {
-    render(
-      <ModalProvider>
-        <Categories />
-      </ModalProvider>
-    )
+    renderWithProviders(<Categories />)
 
     expect(await screen.findByText('Categories')).toBeInTheDocument()
   })
