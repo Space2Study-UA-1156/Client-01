347a546f0fb75c9e098500d9144fdfd71a78aded
holus-bolus <ustinovoleksij@gmail.com>
2024-05-02 14:16:29 +0300

    Added the general info step

--- src/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles.js
+++ src/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles.js
@@ -1,11 +1,51 @@
-import { fadeAnimation } from '~/styles/app-theme/custom-animations'
+import { makeStyles } from '@mui/styles'
 
-export const styles = {
-  container: {
+const useStyles = makeStyles(() => ({
+  root: {
     display: 'flex',
-    justifyContent: 'space-around',
-    height: { sm: '485px' },
-    paddingBottom: { xs: '30px', sm: '0' },
-    ...fadeAnimation
+    justifyContent: 'space-between',
+    gap: '115px'
+  },
+  imageContainer: {
+    width: '380px'
+  },
+  formContainer: {
+    width: '450px',
+    borderRadius: 0,
+    boxShadow: 'none',
+    height: '100%'
+  },
+  row: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginBottom: '20px',
+    gap: '20px'
+  },
+  fullWidthInput: {
+    width: '100%'
+  },
+  halfWidthInput: {
+    width: '100%'
+  },
+  flexBox: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  imageItem: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '380px'
+  },
+  formItem: {
+    display: 'flex',
+    height: '100%',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '380px'
   }
-}
+}))
+
+export default useStyles
