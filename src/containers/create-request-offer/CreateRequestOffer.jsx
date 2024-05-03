3e5e36182a81dcc2dfde4fa14b98cdd5657c278d
Vitalii Voloshyn <voloshin.v.02@gmail.com>
2024-05-03 15:07:26 +0300

    show AppDrawer on button click

--- src/containers/create-request-offer/CreateRequestOffer.jsx
+++ src/containers/create-request-offer/CreateRequestOffer.jsx
@@ -1,18 +1,25 @@
 import { useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 import useBreakpoints from '~/hooks/use-breakpoints'
+import { useDrawer } from '~/hooks/use-drawer'
 
 import Box from '@mui/material/Box'
 import AppButton from '~/components/app-button/AppButton'
 import TitleWithDescription from '~/components/title-with-description/TitleWithDescription'
+import AppDrawer from '~/components/app-drawer/AppDrawer'
 
-import subjectIcon from '~/assets/img/find-offers/subject_icon.svg'
-import { styles } from '~/containers/find-offers/create-request-offer/CreateRequestOffer.styles'
+import subjectIcon from '~/assets/img/create-request-offer/subject_icon.svg'
+import { styles } from '~/containers/create-request-offer/CreateRequestOffer.styles'
 
 const CreateRequestOffer = () => {
   const { t } = useTranslation()
   const { userRole } = useSelector((state) => state.appMain)
   const { isMobile } = useBreakpoints()
+  const { isOpen, openDrawer, closeDrawer } = useDrawer()
+
+  const handleOpen = () => {
+    openDrawer()
+  }
 
   return (
     <Box component='section' sx={styles.root}>
@@ -24,13 +31,17 @@ const CreateRequestOffer = () => {
             title={t(`findOffers.offerRequestBlock.title.${userRole}`)}
           />
 
-          <AppButton sx={styles.button}>
+          <AppButton onClick={handleOpen} sx={styles.button}>
             {t(`findOffers.offerRequestBlock.button.${userRole}`)}
           </AppButton>
         </Box>
 
         {!isMobile && <Box alt='subject' component='img' src={subjectIcon} />}
       </Box>
+
+      <AppDrawer onClose={closeDrawer} open={isOpen}>
+        {/* TODO: add a new request/offer form */}
+      </AppDrawer>
     </Box>
   )
 }
