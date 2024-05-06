acf3b22f297892309bde4a316aeb22191327fb07
OlhaOvsiienko <o.v.ovsiyenko@gmail.com>
2024-05-06 12:58:09 +0300

    email confirm popup window

---
+++ src/containers/email-confirm-popup-window/EmailConfirmPopupWindow.jsx
@@ -0,0 +1,53 @@
+import { Box, IconButton } from '@mui/material'
+import ImgTitleDescription from '~/components/img-title-description/ImgTitleDescription'
+import AppButton from '~/components/app-button/AppButton'
+import imgSuccess from '~/assets/img/email-confirmation-modals/success-icon.svg'
+import LoginDialog from '~/containers/guest-home-page/login-dialog/LoginDialog'
+import { useModalContext } from '~/context/modal-context'
+import { useTranslation } from 'react-i18next'
+
+import { styles } from '~/containers/email-confirm-popup-window/EmailConfirmPopupWindow.styles'
+
+const EmailConfirmPopupWindow = () => {
+    const { t } = useTranslation()
+    const { closeModal, openModal } = useModalContext()
+
+    const handleClose = () => {
+        closeModal();
+    }
+
+    const openLoginDialog = () => {
+        openModal({ component: <LoginDialog /> })
+    }
+
+    return (
+        <Box 
+            data-testid="modal-root"
+            sx={styles.box} 
+        >
+            <IconButton 
+                onClick={handleClose}
+                sx={styles.closeButton} 
+                aria-label="Close"
+            />
+            <ImgTitleDescription
+                img={imgSuccess}
+                alt="Success Icon"
+                style={styles}
+                title={t('modals.emailConfirm')}
+            />
+            <AppButton
+                onClick={openLoginDialog}
+                data-testid="go-to-login-button"
+                size={'large'}
+                sx={styles.button}
+                type='submit'
+                variant='contained'
+            >
+                {t('button.goToLogin')}
+            </AppButton>
+        </Box>
+    )
+}
+
+export default EmailConfirmPopupWindow
