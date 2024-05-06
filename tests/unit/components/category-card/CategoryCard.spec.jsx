8f3a5de2f86597cface4375a7700657f98cc9a1e
YaroslavChuiko <jarik.tchuicko@gmail.com>
2024-05-06 20:10:03 +0300

    Added test for the routing

--- tests/unit/components/category-card/CategoryCard.spec.jsx
+++ tests/unit/components/category-card/CategoryCard.spec.jsx
@@ -1,6 +1,8 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { renderWithProviders } from '../../../test-utils'
 import CategoryCard from '~/components/category-card/CategoryCard'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
+import { authRoutes } from '~/router/constants/authRoutes'
 
 describe('InfoCard component', () => {
   const props = {
@@ -10,25 +12,46 @@ describe('InfoCard component', () => {
     totalOffers: '234'
   }
 
-  beforeEach(() => {
-    renderWithProviders(<CategoryCard {...props} />)
-  })
-
   it('should contain image', () => {
-    const img = screen.getByRole('img')
+    renderWithProviders(<CategoryCard {...props} />)
 
+    const img = screen.getByRole('img')
     expect(img).toBeInTheDocument()
   })
 
   it('should have correct title', () => {
-    const text = screen.getByText(props.title)
+    renderWithProviders(<CategoryCard {...props} />)
 
+    const text = screen.getByText(props.title)
     expect(text).toBeInTheDocument()
   })
 
   it('should have correct description', () => {
-    const text = screen.getByText(`${props.totalOffers} categoriesPage.offers`)
+    renderWithProviders(<CategoryCard {...props} />)
 
+    const text = screen.getByText(`${props.totalOffers} categoriesPage.offers`)
     expect(text).toBeInTheDocument()
+  })
+
+  it('should navigate to the provided path', async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route element={<CategoryCard {...props} />} path='/' />
+          <Route
+            element={<p>subjects page</p>}
+            path={`${authRoutes.subjects.path}/:id`}
+          />
+        </Routes>
+      </MemoryRouter>
+    )
+
+    expect(screen.queryByText(/subjects/i)).not.toBeInTheDocument()
+
+    fireEvent.click(screen.getByRole('img'))
+
+    await waitFor(() => {
+      expect(screen.getByText(/subjects/i)).toBeInTheDocument()
+    })
   })
 })
