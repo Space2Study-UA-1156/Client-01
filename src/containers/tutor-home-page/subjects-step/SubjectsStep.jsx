8e964a6f2ab37084c872b5ab5dc030238016299e
Vadym Khytryk <khytrykv@gmail.com>
2024-05-07 18:07:11 +0300

    Add button and chips to Subjects step (#182)
     Add button and chips to Subjects/Interests step

--- src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
+++ src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
@@ -1,24 +1,32 @@
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import useBreakpoints from '~/hooks/use-breakpoints'
-
-import { categoryService } from '~/services/category-service'
-import { subjectService } from '~/services/subject-service'
-
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-import AsyncAutocomplete from '~/components/async-autocomlete/AsyncAutocomplete'
 
+import useBreakpoints from '~/hooks/use-breakpoints'
+import { categoryService } from '~/services/category-service'
+import { subjectService } from '~/services/subject-service'
+import AsyncAutocomplete from '~/components/async-autocomlete/AsyncAutocomplete'
+import AppButton from '~/components/app-button/AppButton'
+import AppChipList from '~/components/app-chips-list/AppChipList'
 import studyCategory from '~/assets/img/tutor-home-page/become-tutor/study-category.svg'
 import { styles } from '~/containers/tutor-home-page/subjects-step/SubjectsStep.styles'
+import { useStepContext } from '~/context/step-context'
+import { useSelector } from 'react-redux'
+import { student } from '~/constants'
+import { interests, subjects } from '~/components/user-steps-wrapper/constants'
 
 const SubjectsStep = ({ btnsBox }) => {
+  const { userRole } = useSelector((state) => state.appMain)
   const { t } = useTranslation()
   const { isMobile, isLaptopAndAbove } = useBreakpoints()
   const [category, setCategory] = useState(null)
   const [subject, setSubject] = useState(null)
-
+  const { stepData, handleStepData } = useStepContext()
+  const subjectLabel = userRole === student ? interests : subjects
+  const selectedSubjects = stepData[subjectLabel]
   const isCategorySelected = Boolean(category)
+  const isSubjectSelected = !subject
 
   const handleChangeCategory = (e, categoryValue) => {
     setCategory(categoryValue)
@@ -27,6 +35,24 @@ const SubjectsStep = ({ btnsBox }) => {
 
   const handleChangeSubject = (e, subjectValue) => {
     setSubject(subjectValue)
+  }
+
+  const handleAddSubject = () => {
+    if (!subject) return
+    if (selectedSubjects.find((item) => item.name === subject.name)) {
+      setSubject(null)
+      return
+    }
+
+    handleStepData(subjectLabel, [...selectedSubjects, subject])
+    setSubject(null)
+  }
+
+  const handleDeleteSubject = (subjectName) => {
+    handleStepData(
+      subjectLabel,
+      selectedSubjects.filter((subject) => subject.name !== subjectName)
+    )
   }
 
   const imageBox = (
@@ -71,6 +97,20 @@ const SubjectsStep = ({ btnsBox }) => {
             }}
             value={subject?._id}
             valueField='_id'
+          />
+
+          <AppButton
+            disabled={isSubjectSelected}
+            onClick={handleAddSubject}
+            variant={'tonal'}
+          >
+            {t('becomeTutor.categories.btnText')}
+          </AppButton>
+
+          <AppChipList
+            defaultQuantity={2}
+            handleChipDelete={handleDeleteSubject}
+            items={selectedSubjects.map((subject) => subject.name)}
           />
         </Box>
 
