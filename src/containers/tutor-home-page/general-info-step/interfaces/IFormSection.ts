d3d4d91b9a6e1b73f812515369015f8fc123977b
Oleksii Ustinov <93076661+holus-bolus@users.noreply.github.com>
2024-05-08 07:03:21 +0300

    Added the interface for the form section

---
+++ src/containers/tutor-home-page/general-info-step/interfaces/IFormSection.ts
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export interface FormSectionProps {
+  btnsBox: React.ReactNode
+}
+
+export interface StepContext {
+  toggleNextButton: (value: boolean) => void
+  isOverEighteen: boolean
+  handleOverEighteenChange: (value: boolean) => void
+}
+
+export interface GeneralData {
+  data: Record<string, unknown>
+  errors: Record<string, unknown>
+}
+
+export interface StepData {
+  general: GeneralData
+  [key: string]: GeneralData | null
+}
+
+export interface StepContextType {
+  stepData: StepData
+  handleStepData: (
+    stepLabel: string,
+    newData: unknown,
+    newErrors?: Record<string, unknown>
+  ) => void
+  isNextDisabled: boolean
+  toggleNextButton: (disabled: boolean) => void
+  isOverEighteen: boolean
+  handleOverEighteenChange: (value: boolean) => void
+}
