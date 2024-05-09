93706a9d00a2c55169bad639b607cd4d652c180c
Vitalii Voloshyn <56509285+voloshkin-v@users.noreply.github.com>
2024-05-09 18:16:23 +0300

    Photo step (#193)
    * add photo step
    
    * add StepProvider for unit tests
    
    * fix styles, create filePreview element
    
    * refactor component
    
    * add unit tests
    
    * sort imports

--- tests/unit/containers/tutor-home-page/add-photo-step/AddPhotoStep.spec.jsx
+++ tests/unit/containers/tutor-home-page/add-photo-step/AddPhotoStep.spec.jsx
@@ -1,8 +1,40 @@
-import { screen } from '@testing-library/react'
+import { screen, render } from '@testing-library/react'
+import { vi } from 'vitest'
+import { StepProvider, useStepContext } from '~/context/step-context'
+import {
+  initialValues,
+  tutorStepLabels
+} from '~/components/user-steps-wrapper/constants'
 import AddPhotoStep from '~/containers/tutor-home-page/add-photo-step/AddPhotoStep'
-import { renderWithProviders } from '~tests/test-utils'
+
+vi.mock('~/hooks/use-breakpoints', () => ({
+  default: () => ({
+    isLaptopAndAbove: true
+  })
+}))
+
+vi.mock('~/context/step-context', async (importOriginal) => {
+  const mod = await importOriginal()
+
+  return {
+    ...mod,
+    useStepContext: vi.fn(() => ({
+      stepData: { photo: [] }
+    }))
+  }
+})
+
+vi.mock('~/components/file-uploader/FileUploader', () => ({
+  default: ({ buttonText }) => <button>{buttonText}</button>
+}))
+
+vi.mock('~/components/drag-and-drop/DragAndDrop', () => ({
+  default: () => <div>DragAndDrop</div>
+}))
 
 describe('AddPhotoStep component test', () => {
+  window.URL.createObjectURL = vi.fn()
+
   const btnsBox = (
     <>
       <button data-testid='btn1' />
@@ -10,16 +42,63 @@ describe('AddPhotoStep component test', () => {
     </>
   )
 
-  beforeEach(() => {
-    renderWithProviders(<AddPhotoStep btnsBox={btnsBox} />)
+  it('should render container', () => {
+    render(
+      <StepProvider initialValues={initialValues} stepLabels={tutorStepLabels}>
+        <AddPhotoStep btnsBox={btnsBox} />
+      </StepProvider>
+    )
+
+    const uploadButton = screen.getByText('becomeTutor.photo.button')
+    expect(uploadButton).toBeInTheDocument()
   })
 
-  it('should render container', () => {
-    const AddPhotoStepContainer = screen.getByText('AddPhoto step')
-    expect(AddPhotoStepContainer).toBeInTheDocument()
-  })
   it('buttons passed in props should be in the document', () => {
+    render(
+      <StepProvider initialValues={initialValues} stepLabels={tutorStepLabels}>
+        <AddPhotoStep btnsBox={btnsBox} />
+      </StepProvider>
+    )
+
     expect(screen.getByTestId('btn1')).toBeInTheDocument()
     expect(screen.getByTestId('btn2')).toBeInTheDocument()
+  })
+
+  it('should render photo preview if file is selected', () => {
+    const mockedContextValue = {
+      stepData: {
+        photo: [{ name: 'image.jpg' }]
+      }
+    }
+    useStepContext.mockReturnValue(mockedContextValue)
+
+    render(
+      <StepProvider initialValues={initialValues} stepLabels={tutorStepLabels}>
+        <AddPhotoStep btnsBox={btnsBox} />
+      </StepProvider>
+    )
+
+    const photoPreview = screen.getByAltText('becomeTutor.photo.imageAlt')
+
+    expect(photoPreview).toBeInTheDocument()
+  })
+
+  it('should render DragAndDrop component if file is not selected', () => {
+    const mockedContextValue = {
+      stepData: {
+        photo: []
+      }
+    }
+    useStepContext.mockReturnValue(mockedContextValue)
+
+    render(
+      <StepProvider initialValues={initialValues} stepLabels={tutorStepLabels}>
+        <AddPhotoStep btnsBox={btnsBox} />
+      </StepProvider>
+    )
+
+    const dragEndDrop = screen.getByText('DragAndDrop')
+
+    expect(dragEndDrop).toBeInTheDocument()
   })
 })
