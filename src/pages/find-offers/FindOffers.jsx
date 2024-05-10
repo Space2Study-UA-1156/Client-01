4feafb0ddb967db49bfd6640751d6f961cbab672
YaroslavLys <74725159+YaroslavLys@users.noreply.github.com>
2024-05-10 15:53:39 +0300

    Value view was put in search params

--- src/pages/find-offers/FindOffers.jsx
+++ src/pages/find-offers/FindOffers.jsx
@@ -1,10 +1,16 @@
-import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import { useEffect, useState } from 'react'
 import Stack from '@mui/material/Stack'
+import { useSearchParams } from 'react-router-dom'
+import PageWrapper from '~/components/page-wrapper/PageWrapper'
 import AppViewSwitcher from '~/components/app-view-switcher/AppViewSwitcher'
-import { useState } from 'react'
 
 const FindOffers = () => {
-  const [view, setView] = useState('list')
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [view, setView] = useState(searchParams.get('view') || 'list')
+
+  useEffect(() => {
+    setSearchParams({ view })
+  }, [view, setSearchParams])
 
   return (
     <PageWrapper>
