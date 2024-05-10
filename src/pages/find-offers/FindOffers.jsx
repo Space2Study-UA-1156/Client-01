ab19f3c9069c6ecd82a62d65eb2066d3bf375832
YaroslavLys <74725159+YaroslavLys@users.noreply.github.com>
2024-05-10 13:24:43 +0300

    Created AppViewSwitcher component

--- src/pages/find-offers/FindOffers.jsx
+++ src/pages/find-offers/FindOffers.jsx
@@ -1,7 +1,19 @@
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import Stack from '@mui/material/Stack'
+import AppViewSwitcher from '~/components/app-view-switcher/AppViewSwitcher'
+import { useState } from 'react'
 
 const FindOffers = () => {
-  return <PageWrapper>Find offers</PageWrapper>
+  const [view, setView] = useState('list')
+
+  return (
+    <PageWrapper>
+      Find offers
+      <Stack>
+        <AppViewSwitcher setView={setView} view={view} />
+      </Stack>
+    </PageWrapper>
+  )
 }
 
 export default FindOffers
