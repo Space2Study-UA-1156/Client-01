2520a06203a9230b5f58f97940bd31504bfa2948
Oleksii Ustinov <93076661+holus-bolus@users.noreply.github.com>
2024-05-13 17:12:57 +0300

    Added the interface for the form section (#195)
    * Added the interface for the form section
    
    * Fixed the eslint error
    
    * Fixed the eslint error
    
    * Disabled the confirm

--- src/containers/tutor-home-page/general-info-step/FormSection.tsx
+++ src/containers/tutor-home-page/general-info-step/FormSection.tsx
@@ -2,16 +2,17 @@ import React, { useState } from 'react'
 import { Typography, Checkbox, FormControlLabel, Box } from '@mui/material'
 import TextFieldGroup from './TextFieldGroup'
 import { useStepContext } from '~/context/step-context'
-
-interface FormSectionProps {
-  btnsBox: React.ReactNode
-}
+import {
+  FormSectionProps,
+  StepContextType
+} from '~/containers/tutor-home-page/general-info-step/interfaces/IFormSection'
 
 const FormSection: React.FC<FormSectionProps> = ({ btnsBox }) => {
   const [message, setMessage] = useState('')
+  // eslint-disable-next-line
   const [isConfirmed, setIsConfirmed] = useState(false)
   const { toggleNextButton, isOverEighteen, handleOverEighteenChange } =
-    useStepContext()
+    useStepContext() as StepContextType
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const message = event.target.value.slice(0, 100)
     setMessage(message)
