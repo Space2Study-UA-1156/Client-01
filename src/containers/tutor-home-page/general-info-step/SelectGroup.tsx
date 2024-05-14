c1bb0609a65d8317cfaf4c2529756627255784aa
Oleksii Ustinov <93076661+holus-bolus@users.noreply.github.com>
2024-05-14 07:57:47 +0300

    Added the cities and countries fetching

--- src/containers/tutor-home-page/general-info-step/SelectGroup.tsx
+++ src/containers/tutor-home-page/general-info-step/SelectGroup.tsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FormControl, InputLabel, Select, MenuItem } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 import { useSelectGroupStyles } from './SelectGroup.styles'
+import { LocationService } from '~/services/location-service'
+interface Country {
+  name: string
+}
 
+interface City {
+  name: string
+}
 const SelectGroup: React.FC = () => {
   const { t } = useTranslation()
   const classes = useSelectGroupStyles()
+  const [countries, setCountries] = useState([])
+  const [cities, setCities] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState('')
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await LocationService.getCountries()
+        setCountries(response.data as Country[])
+      } catch (error) {
+        console.error('Error fetching countries:', error)
+      }
+    }
+
+    void fetchCountries()
+  }, [])
+
+  useEffect(() => {
+    if (selectedCountry) {
+      const fetchCities = async () => {
+        try {
+          const response = await LocationService.getCities(selectedCountry)
+          setCities(response.data as City[])
+        } catch (error) {
+          console.error('Error fetching cities:', error)
+        }
+      }
+
+      void fetchCities()
+    }
+  }, [selectedCountry])
+
+  const handleCountryChange = (event: {
+    target: { value: React.SetStateAction<string> }
+  }) => {
+    setSelectedCountry(event.target.value)
+  }
 
   return (
     <div
@@ -13,18 +57,22 @@ const SelectGroup: React.FC = () => {
     >
       <FormControl className={classes.formControl}>
         <InputLabel>{t('common.labels.country')}</InputLabel>
-        <Select label={t('labels.country')}>
-          <MenuItem value='Ukraine'>Ukraine</MenuItem>
-          <MenuItem value='Deutschland'>Deutschland</MenuItem>
-          <MenuItem value='Lithuania'>Lithuania</MenuItem>
+        <Select label={t('labels.country')} onChange={handleCountryChange}>
+          {countries.map((country) => (
+            <MenuItem key={country} value={country}>
+              {country}
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
       <FormControl className={classes.formControl}>
         <InputLabel>{t('common.labels.city')}</InputLabel>
         <Select label={t('labels.city')}>
-          <MenuItem value='Kyiv'>Kyiv</MenuItem>
-          <MenuItem value='Berlin'>Berlin</MenuItem>
-          <MenuItem value='Vilnius'>Vilnius</MenuItem>
+          {cities.map((city) => (
+            <MenuItem key={city} value={city}>
+              {city}
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
     </div>
