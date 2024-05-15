b7ef731254d48687e64203969b4b3e245501ef43
Oleksii Ustinov <93076661+holus-bolus@users.noreply.github.com>
2024-05-15 10:35:48 +0300

    Added the cities and countries fetching (#219)
    * Added the cities and countries fetching
    
    * Adjusted the selects
    
    * Adjusted the code accordingly to Vlad suggestions

--- src/containers/tutor-home-page/general-info-step/SelectGroup.tsx
+++ src/containers/tutor-home-page/general-info-step/SelectGroup.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
-import { FormControl, InputLabel, Select, MenuItem } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  SelectChangeEvent
+} from '@mui/material'
 import { useTranslation } from 'react-i18next'
 import { useSelectGroupStyles } from './SelectGroup.styles'
+import { LocationService } from '~/services/location-service'
 
 const SelectGroup: React.FC = () => {
   const { t } = useTranslation()
   const classes = useSelectGroupStyles()
+  const [countries, setCountries] = useState<string[]>([])
+  const [cities, setCities] = useState<string[]>([])
+  const [selectedCountry, setSelectedCountry] = useState('')
+  const [selectedCity, setSelectedCity] = useState('')
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await LocationService.getCountries()
+        setCountries(response.data as string[])
+      } catch (error) {
+        console.error('Error fetching countries:', error)
+      }
+    }
+
+    void fetchCountries()
+  }, [])
+
+  useEffect(() => {
+    if (selectedCountry) {
+      const fetchCities = async () => {
+        try {
+          const response = await LocationService.getCities(selectedCountry)
+          setCities(response.data as string[])
+        } catch (error) {
+          console.error('Error fetching cities:', error)
+        }
+      }
+
+      void fetchCities()
+    } else {
+      setCities([])
+      setSelectedCity('')
+    }
+  }, [selectedCountry])
+
+  const handleCountryChange = (event: SelectChangeEvent) => {
+    setSelectedCountry(event.target.value)
+  }
+
+  const handleCityChange = (event: SelectChangeEvent) => {
+    setSelectedCity(event.target.value)
+  }
 
   return (
     <div
@@ -13,18 +63,31 @@ const SelectGroup: React.FC = () => {
     >
       <FormControl className={classes.formControl}>
         <InputLabel>{t('common.labels.country')}</InputLabel>
-        <Select label={t('labels.country')}>
-          <MenuItem value='Ukraine'>Ukraine</MenuItem>
-          <MenuItem value='Deutschland'>Deutschland</MenuItem>
-          <MenuItem value='Lithuania'>Lithuania</MenuItem>
+        <Select
+          label={t('labels.country')}
+          onChange={handleCountryChange}
+          value={selectedCountry}
+        >
+          {countries.map((country) => (
+            <MenuItem key={country} value={country}>
+              {country}
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
       <FormControl className={classes.formControl}>
         <InputLabel>{t('common.labels.city')}</InputLabel>
-        <Select label={t('labels.city')}>
-          <MenuItem value='Kyiv'>Kyiv</MenuItem>
-          <MenuItem value='Berlin'>Berlin</MenuItem>
-          <MenuItem value='Vilnius'>Vilnius</MenuItem>
+        <Select
+          disabled={!selectedCountry}
+          label={t('labels.city')}
+          onChange={handleCityChange}
+          value={selectedCity}
+        >
+          {cities.map((city) => (
+            <MenuItem key={city} value={city}>
+              {city}
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
     </div>
