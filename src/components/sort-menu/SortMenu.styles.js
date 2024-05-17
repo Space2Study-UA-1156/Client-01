1daeb8b868aff857621cc044126d8b9623df0f32
OlhaOvsiienko <137906412+OlhaOvsiienko@users.noreply.github.com>
2024-05-17 09:34:39 +0300

    Sort Menu component with tests (#224)
    * Sort Menu component with tests after correction comments

---
+++ src/components/sort-menu/SortMenu.styles.js
@@ -0,0 +1,32 @@
+export const styles = {
+  root: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '185px',
+    height: '48px',
+    fontSize: '16px',
+    fontFamily: 'Rubik',
+    fontWeight: 400
+  },
+
+  label: {
+    width: '62px',
+    height: '24px',
+    color: 'primary.400',
+    fontWeight: 400,
+    display: 'flex',
+    alignItems: 'center'
+  },
+
+  sortContainer: {
+    width: '115px'
+  },
+
+  buttonSort: {
+    color: '#263238',
+    fontWeight: 400,
+    padding: 0,
+    margin: 0
+  }
+}
---
+++ tests/unit/components/sort-menu/SortMenu.spec.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { screen, fireEvent } from '@testing-library/react'
+import SortMenu from '~/components/sort-menu/SortMenu'
+import { expect, vi } from 'vitest'
+import { renderWithProviders } from '~tests/test-utils'
+
+const mockSetSearchParams = vi.fn()
+
+vi.mock('react-router-dom', async (importOriginal) => {
+  const module = await importOriginal()
+
+  return {
+    ...module,
+    useSearchParams: () => [{ get: vi.fn() }, mockSetSearchParams]
+  }
+})
+
+vi.mock('@mui/material/Select', () => {
+  const Select = ({ children, onChange, value, ...props }) => {
+    return (
+      <div role='combobox' {...props} data-value={value}>
+        {React.Children.map(children, (child) =>
+          React.cloneElement(child, {
+            onClick: () =>
+              onChange && onChange({ target: { value: child.props.value } })
+          })
+        )}
+      </div>
+    )
+  }
+  return {
+    __esModule: true,
+    default: Select
+  }
+})
+
+vi.mock('@mui/material/MenuItem', () => ({
+  __esModule: true,
+  default: ({ children, value, ...props }) => (
+    <div data-value={value} role='option' {...props}>
+      {children}
+    </div>
+  )
+}))
+
+vi.mock('~/components/app-button/AppButton', () => ({
+  __esModule: true,
+  default: ({ data_testid, children }) => (
+    <button data-testid={data_testid}>{children}</button>
+  )
+}))
+
+describe('SortMenu', () => {
+  it('dropdown appears with 4 sorting options when sort button is clicked', () => {
+    renderWithProviders(<SortMenu />)
+    const selectElement = screen.getByRole('combobox')
+
+    fireEvent.mouseDown(selectElement)
+
+    expect(screen.getByTestId('button-newest')).toBeInTheDocument()
+    expect(screen.getByTestId('button-rating')).toBeInTheDocument()
+    expect(screen.getByTestId('button-priceAsc')).toBeInTheDocument()
+    expect(screen.getByTestId('button-priceDesc')).toBeInTheDocument()
+  })
+
+  it('selecting an option updates searchParams and displays the selected value', () => {
+    renderWithProviders(<SortMenu />)
+
+    const selectElement = screen.getByRole('combobox')
+    fireEvent.mouseDown(selectElement)
+
+    const ratingOption = screen.getByTestId('button-rating')
+    fireEvent.click(ratingOption)
+
+    assert(() => {
+      expect(mockSetSearchParams).toHaveBeenCalledWith({ sort: 'rating' })
+    })
+
+    expect(screen.getByRole('combobox')).toHaveAttribute('data-value', 'rating')
+  })
+})
