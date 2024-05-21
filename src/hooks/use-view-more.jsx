a0d6e09a196f291a5211ddfd312a0cf28d77abaa
Vitalii Voloshyn <voloshin.v.02@gmail.com>
2024-05-21 17:38:12 +0300

    create reusable useViewMore hook, add CardList component with default styles

---
+++ src/hooks/use-view-more.jsx
@@ -0,0 +1,59 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import useAxios from '~/hooks/use-axios'
+
+const useViewMore = ({
+  service,
+  searchParamKey = 'page',
+  cardsPerPage = 1
+}) => {
+  const [data, setData] = useState([])
+  const [searchParams, setSearchParams] = useSearchParams()
+  const page = Number(searchParams.get(searchParamKey)) || 1
+  const isPageInSearchParams = searchParams.has(searchParamKey)
+
+  useEffect(() => {
+    if (isPageInSearchParams) {
+      fetchData({ limit: page * cardsPerPage })
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const onResponse = useCallback((response) => {
+    setData((prev) => [...prev, ...response.items])
+  }, [])
+
+  const serviceFn = useCallback(
+    (params) => service({ limit: cardsPerPage, ...params }),
+    [service, cardsPerPage]
+  )
+
+  const { response, fetchData, error, loading } = useAxios({
+    service: serviceFn,
+    onResponse,
+    defaultResponse: {},
+    fetchOnMount: !isPageInSearchParams
+  })
+
+  const isViewMoreVisable = response?.count > cardsPerPage * page
+
+  const handleViewMore = () => {
+    const nextPage = page + 1
+    const skip = (nextPage - 1) * cardsPerPage
+
+    searchParams.set(searchParamKey, nextPage)
+    setSearchParams(searchParams)
+
+    fetchData({ skip })
+  }
+
+  return {
+    isViewMoreVisable,
+    data,
+    handleViewMore,
+    loading,
+    error
+  }
+}
+
+export default useViewMore
