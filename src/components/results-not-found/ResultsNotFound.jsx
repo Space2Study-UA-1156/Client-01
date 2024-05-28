18c6ad9de75e830a13871e3f04e7b72f7f8ed53c
Vitalii Voloshyn <56509285+voloshkin-v@users.noreply.github.com>
2024-05-28 11:41:41 +0300

    reusable ResultsNotFound component (#299)
    * change CategoriesResultsNotFound to reusable ResultsNotFound component
    
    * add prop showButton

---
+++ src/components/results-not-found/ResultsNotFound.jsx
@@ -0,0 +1,49 @@
+import { useTranslation } from 'react-i18next'
+
+import Box from '@mui/material/Box'
+import ImgTitleDescription from '~/components/img-title-description/ImgTitleDescription'
+import AppButton from '~/components/app-button/AppButton'
+
+import { useModalContext } from '~/context/modal-context'
+import { spliceSx } from '~/utils/helper-functions'
+import { styles } from '~/components/results-not-found/ResultsNotFound.styles'
+import Magnifier from '~/assets/img/guest-home-page/howItWorksStudentSecond.svg'
+
+const ResultsNotFound = ({
+  name = 'category',
+  dialog = <p>default dialog</p>, // TODO
+  sx = {},
+  showButton = true
+}) => {
+  const { t } = useTranslation()
+  const { openModal } = useModalContext()
+
+  const handleOpenModal = () => {
+    openModal({ component: dialog })
+  }
+
+  return (
+    <Box component='section' sx={spliceSx(styles.root, sx.root)}>
+      <Box sx={spliceSx(styles.box, sx.box)}>
+        <ImgTitleDescription
+          description={t('constant.tryAgainText', { name })}
+          img={Magnifier}
+          style={styles.content}
+          title={t('constant.resultsNotFound')}
+        />
+
+        {showButton && (
+          <AppButton
+            onClick={handleOpenModal}
+            sx={styles.button}
+            variant='tonal'
+          >
+            {t('constant.buttonRequest', { name })}
+          </AppButton>
+        )}
+      </Box>
+    </Box>
+  )
+}
+
+export default ResultsNotFound
