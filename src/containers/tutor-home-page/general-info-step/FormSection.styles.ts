49df6ef4d59a7a6f8bc4ccb547194cb3f3a0d87f
Oleksii Ustinov <93076661+holus-bolus@users.noreply.github.com>
2024-05-30 21:21:05 +0300

    Restored the develop branch

--- src/containers/tutor-home-page/general-info-step/FormSection.styles.ts
+++ src/containers/tutor-home-page/general-info-step/FormSection.styles.ts
@@ -1,12 +1,28 @@
-import { Theme, makeStyles } from '@mui/material/styles'
+import { makeStyles } from '@mui/styles'
+import { Theme } from '@mui/material/styles'
 
 export const useFormSectionStyles = makeStyles((theme: Theme) => ({
   formContainer: {
     width: '100%',
-    maxWidth: '435px',
+    minWidth: '435px',
+    minHeight: '430px',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     padding: theme.spacing(2)
+  },
+  topFormText: {
+    marginBottom: '20px'
+  },
+  formSection: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    minWidth: '435px'
+  },
+  contentBox: {
+    width: '100%',
+    maxWidth: '435px',
+    maxHeight: '415px'
   }
 }))
--- src/containers/tutor-home-page/general-info-step/ImageSection.tsx
+++ src/containers/tutor-home-page/general-info-step/ImageSection.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Box from '@mui/material/Box'
 import generalInfoStepImage from '~/assets/img/tutor-home-page/become-tutor/general-info.svg'
-
 const ImageSection: React.FC = () => {
   return (
     <Box>
