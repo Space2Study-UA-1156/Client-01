29ae5184957dbafd2ea4ffe188468e2163a81215
OlhaOvsiienko <o.v.ovsiyenko@gmail.com>
2024-05-30 23:33:27 +0300

    FindOffers (without tests)

--- src/pages/find-offers/FindOffers.styles.js
+++ src/pages/find-offers/FindOffers.styles.js
@@ -3,5 +3,26 @@ export const styles = {
     display: 'flex',
     justifyContent: 'center',
     flexDirection: { sm: 'row', xs: 'column' }
+  },
+  tutorForPrivateLessons: {
+    marginBottom: '30px'
+  },
+
+  filterSwitcherMenuViewContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: { sm: 'flex-end', md: 'space-between' },
+    alignItems: { sm: 'flex-end', md: 'center' },
+    maxWidth: '1128px',
+    margin: '0 auto',
+    width: '100%',
+    marginTop: '30px',
+    marginBottom: '30px'
+  },
+
+  rootFindOffers: {
+    maxWidth: '1128px',
+    margin: '0 auto',
+    width: '100%'
   }
 }
