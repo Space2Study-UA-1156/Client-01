76d051fd1c9260ea5d37d7805467f2381f598ba6
OlhaOvsiienko <137906412+OlhaOvsiienko@users.noreply.github.com>
2024-05-30 22:56:22 +0300

    Add service for find offers by categoryId, subjectId and other params (#319)
    category-service with tests

--- src/constants/request.js
+++ src/constants/request.js
@@ -33,7 +33,8 @@ export const URLs = {
   categories: {
     get: '/categories',
     getNames: '/categories/names',
-    priceRange: '/price-range'
+    priceRange: '/price-range',
+    getOffers: '/categories/:categoryId/subjects/:subjectId/offers'
   },
   subjects: {
     get: '/subjects',
--- src/services/category-service.js
+++ src/services/category-service.js
@@ -1,5 +1,5 @@
 import { axiosClient } from '~/plugins/axiosClient'
-
+import { createUrlPath } from '~/utils/helper-functions'
 import { URLs } from '~/constants/request'
 
 export const categoryService = {
@@ -8,5 +8,13 @@ export const categoryService = {
   },
   getCategoriesNames: () => {
     return axiosClient.get(URLs.categories.getNames)
+  },
+  getOffers: (data) => {
+    const { categoryId, subjectId, ...params } = data
+    const category = createUrlPath(URLs.categories.get, categoryId)
+    const subject = createUrlPath(URLs.subjects.get, subjectId)
+
+    const fullUrl = `${category}${subject}/offers`
+    return axiosClient.get(fullUrl, { params })
   }
 }
