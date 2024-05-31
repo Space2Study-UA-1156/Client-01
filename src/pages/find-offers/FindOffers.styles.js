ed776964a984bc1bb025c5d350ebccb19ddb173d
OlhaOvsiienko <o.v.ovsiyenko@gmail.com>
2024-05-31 13:29:40 +0300

    FindOffers  Page (whole)

--- src/pages/find-offers/FindOffers.styles.js
+++ src/pages/find-offers/FindOffers.styles.js
@@ -3,5 +3,32 @@ export const styles = {
     display: 'flex',
     justifyContent: 'center',
     flexDirection: { sm: 'row', xs: 'column' }
+  },
+  tutorForPrivateLessons: {
+    marginBottom: '30px'
+  },
+
+  filterSwitcherMenuViewContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    maxWidth: '1128px',
+    margin: '0 auto',
+    width: '100%',
+    marginTop: '30px',
+    marginBottom: '30px'
+  },
+
+  rootFindOffers: {
+    maxWidth: '1128px',
+    margin: '0 auto',
+    width: '100%'
+  },
+
+  offersContainerFindOffersPage: {
+    maxWidth: '1128px',
+    margin: '0 auto',
+    width: '100%'
   }
 }
